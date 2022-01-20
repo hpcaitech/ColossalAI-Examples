a66374c39d2eebe577689bd11cb3312f9f68bbc6
LuGY_mac <gyang_lu@foxmail.com>
2022-01-20 11:37:39 +0800

    Added examples of GPT-2, added dataset tutorials

---
+++ language/gpt_2/gpt2_configs/gpt2_1d.py
@@ -0,0 +1,34 @@
+from colossalai.amp import AMP_TYPE
+from model.gpt import GPTLMLoss, GPT2_small
+from torch.optim import Adam
+
+
+BATCH_SIZE = 1
+SEQ_LEN = 1024
+NUM_EPOCHS = 60
+
+TENSOR_PARALLEL = 2
+
+optimizer = dict(
+    type=Adam,
+    lr=0.00015,
+    weight_decay=1e-2,
+)
+
+fp16 = dict(
+    mode=AMP_TYPE.NAIVE
+)
+
+loss = dict(
+    type=GPTLMLoss,
+)
+
+model = dict(
+    type=GPT2_small,
+    checkpoint=True,
+)
+
+parallel = dict(
+    pipeline=1,
+    tensor=dict(size=TENSOR_PARALLEL, mode='1d'),
+)
