30d2981e3d98abee146943f23ad7ff43b179d74d
FrankLeeeee <somerlee.9@gmail.com>
2022-01-27 16:22:00 +0800

    added engine and trainer script for amp example

--- features/amp/scripts/train_slurm.sh
+++ features/amp/scripts/train_slurm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python train.py --host $HOST --config ./config/config_AMP_naive.py --port 29500
+python train_with_engine.py --host $HOST --config ./config/config_AMP_naive.py --port 29500
