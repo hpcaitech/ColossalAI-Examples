49fade5d8429999f416f98e9d3eda13e31d76477
ver217 <lhx0217@gmail.com>
2022-03-29 20:12:47 +0800

    polish gpt example (#67)

--- language/gpt/train_gpt.py
+++ language/gpt/train_gpt.py
@@ -120,7 +120,8 @@ def main():
         test_interval=1,
         hooks=hook_list,
         display_progress=True,
-        return_output_label=False
+        return_output_label=False,
+        max_steps=100
     )
 
 
