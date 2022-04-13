96193d08457bac7b099439196caa22491b66ca44
XIXIYOUNG2018 <44111169+XIXIYOUNG2018@users.noreply.github.com>
2022-04-13 13:51:55 +0800

    Delete train.sh

--- biology/train.sh
+++
@@ -1,3 +0,0 @@
-#export DATA=$PWD/data
-torchrun --standalone --nnodes=1 --nproc_per_node 1 run_resnet_property_prediction_with_engine.py
-#torchrun --standalone --nnodes=1 --nproc_per_node 1  run_resnet_property_prediction_with_trainer.py
