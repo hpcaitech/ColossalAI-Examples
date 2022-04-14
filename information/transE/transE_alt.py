942cdb8c8abf5ef853e8860e76f1ce904ce792c6
CsRic <mkkt_bkkt@mail.ustc.edu.cn>
2022-04-14 21:40:39 +0800

    done one gpu testing

--- information/transE/transE_alt.py
+++ information/transE/transE_alt.py
@@ -5,9 +5,11 @@ from torch.optim import optimizer
 from torch.utils import data as torch_data
 from colossalai.core import global_context as gpc
 from colossalai.utils import get_dataloader
-
+from colossalai.logging import get_dist_logger
 import colossalai
+from colossalai.nn.lr_scheduler import CosineAnnealingLR
 
+logger = get_dist_logger()
 
 def data_loader(file):
     triple = []
@@ -40,19 +42,20 @@ def test(test_triple, engine, hit_n: int, entity_size: int):
     hit = 0
     total_examples = 0
     for head, relation, tail in test_generator:
+        tail = tail.cuda()
         current_batch_size = head.size()[0]
         entity_ids = torch.arange(end=entity_size).unsqueeze(0)
         all_entities = entity_ids.repeat(current_batch_size, 1)
         heads = head.reshape(-1, 1).repeat(1, all_entities.size()[1])
         relations = relation.reshape(-1, 1).repeat(1, all_entities.size()[1])
-        triplets = torch.stack(
-            (heads, relations, all_entities), dim=2).reshape(-1, 3)
-        tails_predictions = engine(triplets).reshape(current_batch_size, -1)
+        triplets = torch.stack((heads, relations, all_entities), dim=2).reshape(-1, 3)
+        triplets = triplets.cuda()
+        tails_predictions, _ = engine(triplets, torch.tensor([[0,0,0]]).cuda())
+        tails_predictions = tails_predictions.reshape(current_batch_size, -1)
         _, index_predictions = tails_predictions.topk(k=hit_n, largest=False)
-        zero_tensor = torch.tensor([0])
-        one_tensor = torch.tensor([1])
-        hit += torch.where(index_predictions == tail.reshape(-1, 1),
-                           one_tensor, zero_tensor).sum().item()
+        zero_tensor = torch.tensor([0]).cuda()
+        one_tensor = torch.tensor([1]).cuda()
+        hit += torch.where(index_predictions == tail.reshape(-1, 1),one_tensor, zero_tensor).sum().item()
         total_examples += current_batch_size
     return hit / total_examples
 
@@ -103,25 +106,26 @@ class tranETrainer:
         self.max_iteration = max_iteration
         self.entity_size = entity_size
     def train(self):
+        
         for iter in range(self.max_iteration):
             engine.train()
             for real_heads, real_relations, real_tails in self.train_dataloader:
-                positive_triples = torch.stack(
-                    (real_heads, real_relations, real_tails), dim=1)
-                random_entities = torch.randint(
-                    high=self.entity_size, size=real_heads.size())
-                negative_triples = torch.stack(
-                    (real_heads, real_relations, random_entities), dim=1)
-
+                positive_triples = torch.stack((real_heads, real_relations, real_tails), dim=1)
+                random_entities = torch.randint(high=self.entity_size, size=real_heads.size())
+                negative_triples = torch.stack((real_heads, real_relations, random_entities), dim=1)
+                positive_triples = positive_triples.cuda()
+                random_entities = random_entities.cuda()
+                negative_triples = negative_triples.cuda()
+                
                 engine.zero_grad()
-                pos_distance, neg_distance = engine(
-                    positive_triplets=positive_triples, negative_triplets=negative_triples)
-                target = torch.tensor([-1])
+                pos_distance, neg_distance = engine(positive_triplets=positive_triples, negative_triplets=negative_triples)
+                target = torch.tensor([-1]).cuda()
+                
                 loss = engine.criterion(pos_distance, neg_distance, target)
-                engine.backward(loss)
+                engine.backward(loss.mean())
                 engine.step()
-            print(loss.mean())
-
+            logger.info(f"Epoch {iter} - train loss: {loss.mean():.5}")
+            lr_scheduler.step()
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -130,10 +134,11 @@ if __name__ == "__main__":
     triples = data_loader("./train.txt")
     test_triples = data_loader("./dev.txt")
 
-    model = TransE(entity_size=15000, relation_size=240,
-                   embedding_dim=100, norm=2)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    model = TransE(entity_size=15000, relation_size=240, embedding_dim=100, norm=2)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.05)
     criterion = nn.MarginRankingLoss(margin=0.8, reduction='none')
+    lr_scheduler = CosineAnnealingLR(
+        optimizer, total_steps=gpc.config.NUM_EPOCHS)
     head = torch.tensor([int(i[0]) for i in triples])
     rel = torch.tensor([int(i[1]) for i in triples])
     tail = torch.tensor([int(i[2]) for i in triples])
@@ -141,6 +146,8 @@ if __name__ == "__main__":
     train_dataloader = get_dataloader(
         dataset=triples_retrieve, batch_size=gpc.config.BATCH_SIZE, shuffle=True)
 
+    #load_checkpoint('checkpoint.tar', model, optimizer)
+    
     engine, train_dataloader, _, _ = colossalai.initialize(
         model=model, 
         optimizer=optimizer, 
@@ -149,7 +156,9 @@ if __name__ == "__main__":
         test_dataloader=None)
     trainer = tranETrainer(engine, train_dataloader, gpc.config.NUM_EPOCHS)
     trainer.train()
-
+    
+    #save_checkpoint(model, optimizer)
+    logger.info("evaluating...")
     engine.eval()
     hit = test(test_triples, engine, 5, 15000)
-    print(hit)
+    logger.info(f"hit5: {hit}")
