31517936f832445aa004f7e4b9ef58b52eba52a9
YuliangLiu0306 <liuyuliang@luchentech.com>
2022-05-07 23:50:52 +0800

    [titans]/use titans to support knowledge graph

--- image/vilt/run.sh
+++ image/vilt/run.sh
@@ -14,7 +14,7 @@ cd $WORK_DIR
 
 if ! [ -x "$(command -v mpirun)" ]
 then
-    torchrun --nproc_per_node $NUM_GPUS --master_addr localhost --master_port 11455 train.py
+    torchrun --nproc_per_node $NUM_GPUS --master_addr localhost --master_port 11455 run.py
 else
     mpirun -np $NUM_GPUS python run.py with data_root=$DATA_ROOT num_gpus=$NUM_GPUS num_nodes=1 task_mlm_itm_s step200k per_gpu_batchsize=96
 fi
