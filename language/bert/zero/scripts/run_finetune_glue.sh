3d902cbb8440c7fd733672503d7a62caab707ebc
Frank Lee <somerlee.9@gmail.com>
2022-06-20 17:52:29 +0800

    [bert] fixed zero usage (#145)

--- language/bert/zero/scripts/run_finetune_glue.sh
+++ language/bert/zero/scripts/run_finetune_glue.sh
@@ -1,16 +1,15 @@
 
-GLUE_DATASET=/home/lclsg/projects/colossal/github/lsg/ColossalAI-Examples/language/bert/zero/download/glue/MRPC
+GLUE_DATASET=$PWD/download/glue/MRPC
 VOCAB_FILE="bert-base-uncased"
 CODE_DIR=$PWD/finetuning/glue
 
 colossalai run --nproc_per_node 4 \
-    --master_port 29505 \
+    --master_port 29510 \
     $CODE_DIR/main.py \
     --data_dir $GLUE_DATASET \
     --task_name mrpc \
     --bert_config ./configs/bert_base.json \
     --vocab_file $VOCAB_FILE \
-    --init_checkpoint /home/lclsg/projects/DeepLearningExamples/PyTorch/LanguageModeling/BERT/download/nvidia_pretrained_weights/bert_base.pt \
     --output_dir ./out \
     --train_batch_size 32 \
     --eval_batch_size 128 \
@@ -18,13 +17,3 @@ colossalai run --nproc_per_node 4 \
     --train \
     --eval \
     --predict 
-
-    # --init_checkpoint ./ckpt/bert-base-uncased.bin \
-    # --train \
-    # --bert_config ./configs/bert_base.json \
-
-
-
-
-
-    
