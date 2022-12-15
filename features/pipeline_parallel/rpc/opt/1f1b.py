40cf46a812a028fa9bf3479e059fa9c145db3f5e
Ziyue Jiang <ziyue.jiang@gmail.com>
2022-12-15 17:02:36 +0800

    polish code

--- features/pipeline_parallel/rpc/opt/1f1b.py
+++ features/pipeline_parallel/rpc/opt/1f1b.py
@@ -14,12 +14,12 @@ from tqdm.auto import tqdm
 from itertools import chain
 from functools import partial
 from colossalai.fx import ColoTracer
-from colossalai.fx.passes.adding_split_node_pass import split_with_split_nodes_pass, balanced_split_pass
+from colossalai.fx.passes.adding_split_node_pass import split_with_split_nodes_pass, balanced_split_pass, avgnode_split_pass
+from colossalai.pipeline.middleware.adaptor import get_fx_topology
 
 import torch
 import time
 import inspect
-
 '''
 Add following code to colossalai/pipeline/rpc/_pipeline_base.py +497 to make the code work
 if not grad_tensors is None:
@@ -35,7 +35,9 @@ dataset_config = "wikitext-2-raw-v1"
 model_name = "facebook/opt-125m"
 max_block_size = 1024
 
+
 class OPTLoss(torch.nn.Module):
+
     def __init__(self):
         super().__init__()
         self.loss = CrossEntropyLoss()
@@ -46,38 +48,36 @@ class OPTLoss(torch.nn.Module):
         shift_labels = labels[..., 1:].contiguous()
         # Flatten the tokens
         return self.loss(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-    
+
+
 def create_partition_module(pp_rank: int, stage_num: int, model, data_kwargs):
-    model.eval()
     tracer = ColoTracer()
     meta_args = {k: v.to('meta') for k, v in data_kwargs.items()}
     graph = tracer.trace(root=model, meta_args=meta_args)
     gm = torch.fx.GraphModule(model, graph, model.__class__.__name__)
-    annotated_model = balanced_split_pass(gm, stage_num)
-    split_model, split_submodules = split_with_split_nodes_pass(annotated_model)
+    annotated_model = avgnode_split_pass(gm, stage_num)
 
-    return list(split_model.children())[pp_rank]
+    top_module, split_submodules = split_with_split_nodes_pass(annotated_model, merge_output=True)
+    topo = get_fx_topology(top_module)
+    for submodule in split_submodules:
+        if isinstance(submodule, torch.fx.GraphModule):
+            setattr(submodule, '_topo', topo)
+    return split_submodules[pp_rank + 1]
+
 
 def partition(data_kwargs: dict, pp_rank: int, chunk: int, stage_num: int):
-    config = AutoConfig.from_pretrained(model_name)
+    config = AutoConfig.from_pretrained(model_name, local_files_only=True)
+    config.enable_bias = False    # temporarily set it for tracer bug
     model = OPTForCausalLM(config)
     module = create_partition_module(pp_rank, stage_num, model, data_kwargs)
-    
+    num_params = sum(param.numel() for param in module.parameters())
+    print(f'{pp_rank=} {num_params}')
     return module
 
-def data_process_func(pp_rank: int, args_kwargs):
-    if pp_rank == 0:
-        args = args_kwargs[0]
-        kwargs = args_kwargs[1]
-        return args, kwargs
-
-    elif pp_rank == 1:
-        args = list(args_kwargs[:4])
-        return args, {}
 
 def run_master(args):
     logger = get_dist_logger()
-    
+
     batch_size = args.batch_size
     chunk = args.chunk
     device = args.device
@@ -85,15 +85,15 @@ def run_master(args):
     stage_num = world_size
     num_microbatches = args.num_microbatches
     epoch = args.epoch
-    
+
     # prepare dataset
     logger.info("Start preparing dataset", ranks=[0])
     raw_datasets = load_dataset(dataset_path, dataset_config)
     logger.info("Dataset is prepared", ranks=[0])
-    
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=True)
     logger.info(f"{tokenizer.__class__.__name__} has been created", ranks=[0])
-    
+
     # Preprocessing the datasets.
     # First we tokenize all the texts.
     column_names = raw_datasets["train"].column_names
@@ -108,9 +108,9 @@ def run_master(args):
         remove_columns=column_names,
         desc="Running tokenizer on dataset",
     )
-        
+
     block_size = min(max_block_size, tokenizer.model_max_length)
-    
+
     # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
     def group_texts(examples):
         # Concatenate all texts.
@@ -123,7 +123,7 @@ def run_master(args):
         # Split by chunks of max_len.
         result = {
             k: [t[i:i + block_size] for i in range(0, total_length, block_size)
-                ] for k, t in concatenated_examples.items()
+               ] for k, t in concatenated_examples.items()
         }
         result["labels"] = result["input_ids"].copy()
         return result
@@ -136,7 +136,7 @@ def run_master(args):
 
     train_dataset = lm_datasets["train"]
     # eval_dataset = lm_datasets["validation"]
-    
+
     # DataLoaders creation:
     logger.info("Dataloaders is creating", ranks=[0])
     train_dataloader = get_dataloader(train_dataset,
@@ -149,13 +149,12 @@ def run_master(args):
     #                              collate_fn=default_data_collator,
     #                              batch_size=batch_size)
     logger.info("Dataloaders have been created", ranks=[0])
-    
+
     data_kwargs = {},
     for b in train_dataloader:
-        data_kwargs = {'input_ids': b['input_ids'],
-            'attention_mask': b['attention_mask']}
+        data_kwargs = {'input_ids': b['input_ids'], 'attention_mask': b['attention_mask']}
         break
-    
+
     # engine
     engine = OneFOneBPipelineEngine(
         partition_fn=partial(partition, data_kwargs),
@@ -164,19 +163,19 @@ def run_master(args):
         device=device,
         chunk=chunk,
         criterion=OPTLoss(),
-        metric=None,
         checkpoint=False,
-        data_process_func=data_process_func
     )
-    
+
     engine.initialize_optimizer(getattr(torch.optim, args.optimizer), lr=1e-3)
-    
+
+    fwd_cnt = 0
     for b in train_dataloader:
-        batch = {'input_ids': b['input_ids'],
-            'attention_mask': b['attention_mask']}
+        batch = {'input_ids': b['input_ids'], 'attention_mask': b['attention_mask']}
         labels = b['labels']
-        engine.forward_backward(batch=batch, labels=labels)
-        break
+        res = engine.forward_backward(batch=batch, labels=labels, forward_only=False)
+        fwd_cnt += 1
+        print(f'iter: {fwd_cnt} | {res=}')
+
 
 if __name__ == '__main__':
     disable_existing_loggers()
